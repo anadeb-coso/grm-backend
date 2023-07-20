5aa7c05ea9f086b94f3f5b3cbd662e1ae2a4a318
adaboubvincent <adaboubvincent@gmail.com>
2023-07-20 17:51:43 +0000

    Fixed static files problem

--- src/grm/settings.py
+++ src/grm/settings.py
@@ -172,8 +172,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    BASE_DIR / "dashboard/static",
+    BASE_DIR / "static"
     ]
 
 MEDIA_ROOT = BASE_DIR / 'media/'

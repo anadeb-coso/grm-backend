01b8cbf1383bb821c0036352f0a03f1339c4f875
adaboubvincent <adaboubvincent@gmail.com>
2023-09-17 16:45:32 +0000

    Added util function allow to create user

---
+++ src/grm/utils_to_call_on_console.py
@@ -0,0 +1,56 @@
+from authentication.models import User, GovernmentWorker
+from administrativelevels.models import AdministrativeLevel
+from grm.call_objects_from_other_db import mis_objects_call
+from authentication.utils import create_or_update_adl_user_adl
+
+
+def create_training_user(start_number, end_number, administrative_level_type):
+    """
+    Ex: 
+        create_training_user(0, 10, "Canton")
+        create_training_user(7, 10, "Village")
+    """
+
+    administrative_level_filter_by_type = mis_objects_call.filter_objects(
+        AdministrativeLevel, 
+        type=administrative_level_type
+    )
+
+    if not administrative_level_filter_by_type.exists():
+        print(f"Any administrative levels objects exists under type : {administrative_level_type}")
+    else:
+        print("Start saving")
+        print()
+        administrative_level_filter_by_type_values = administrative_level_filter_by_type.values_list('id')
+
+        for number in range(start_number, end_number+1):
+            email = f"training{number}.anadeb@gmail.com"
+            first_name = f"training{number}"
+            last_name = f"training{number}"
+            phone_number = f"228{str(number) * 8}"
+            if not User.objects.filter(email=email).exists():
+                user = User()
+                user.email = email
+                user.first_name = first_name
+                user.last_name = last_name
+                user.phone_number = phone_number
+
+                user.save()
+                user = User.objects.get(email=email)
+
+                government_worker = GovernmentWorker()
+
+                government_worker.user = user
+                government_worker.department = 1
+                try:
+                    adl_id = administrative_level_filter_by_type_values[number-1][0]
+                except:
+                    adl_id = administrative_level_filter_by_type_values[0][0]
+                government_worker.administrative_id = adl_id
+
+                government_worker.save()
+
+                print(f"{email}. Okay")
+
+        print()
+        print("End saving")

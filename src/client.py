eecedb74202e867e49d78dfcff5e3c2cd3713e63
adaboubvincent <adaboubvincent@gmail.com>
2024-03-11 09:58:39 +0000

    Updated client file by adding upload_file_s3 and update_cloudant_document functions

--- src/client.py
+++ src/client.py
@@ -1,5 +1,9 @@
 from cloudant.client import CouchDB
 from django.conf import settings
+from storages.backends.s3boto3 import S3Boto3Storage
+import os
+from datetime import datetime
+import time
 
 COUCHDB_DATABASE = settings.COUCHDB_DATABASE
 COUCHDB_ATTACHMENT_DATABASE = settings.COUCHDB_ATTACHMENT_DATABASE
@@ -32,3 +36,64 @@ def bulk_delete(db_client, documents):
 
 def bulk_update(db_client, edited_documents):
     return db_client.bulk_docs(edited_documents)
+
+
+def update_cloudant_document(db, doc_id, doc_new: dict, dict_of_list_values: dict = {}, attachments=[]):
+    """
+    dict_of_list_values: dict : content as key the attribut of the document who have as value as list and the values of 
+    this key are the attributes than we'll modify their values
+    """
+    _p = {}
+    try:
+        from cloudant.document import Document
+        _p = Document(db, doc_id)
+        
+        for k, v in doc_new.items():
+            if k in list(dict_of_list_values.keys()): #if we have the attributes list to modify
+                attr = []
+                for i in range(len(v)): #Range the interval of the list of the attribut
+                    elt = v[i].copy()
+                    new_elt = v[i].copy()
+
+                    if k == "attachments" and attachments: #if we have the attachments to modify
+                        for elt_attach in attachments: #Go through the attachments
+                            if elt_attach.get("name") and elt_attach.get("name") == elt.get("name"):
+                                elt = attachments[i]
+
+                    for _v in dict_of_list_values[k]: # Go through the attributs list of the doc attr than we going modify
+                        if elt.get(_v):
+                            elt[_v] = new_elt.get(_v)
+                    attr.append(elt)
+
+                _p.field_set(_p, k, attr)
+                continue
+            
+            try:
+                _p.field_set(_p, k, v)
+            except:
+                pass
+            
+        _p.save()
+    except Exception as exc:
+        print(exc)
+        return {}
+    return _p
+
+
+
+def upload_file_s3(file, split=True, sepator="?"):
+    file_directory_within_bucket = 'proof_of_work/'
+    file_path_within_bucket = os.path.join(
+        file_directory_within_bucket,
+        f'{int(time.time())}_{file.name}'
+    )
+
+    media_storage = S3Boto3Storage()
+
+    if not media_storage.exists(file_path_within_bucket):  # avoid overwriting existing file
+        media_storage.save(file_path_within_bucket, file)
+        file_url = media_storage.url(file_path_within_bucket)
+
+        return file_url.split(sepator)[0] if split else file_url
+    
+    return None

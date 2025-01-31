388d11849eb771eb15054a07eb83fa35976d9181
Kodjovi Vincent ADABOUNOU <adaboubvincent@gmail.com>
2025-01-31 15:51:43 +0000

    Added news globals vars

--- src/dashboard/context_processors.py
+++ src/dashboard/context_processors.py
@@ -3,5 +3,11 @@ from django.conf import settings
 
 def settings_vars(request):
     return {
-        'OTHER_LANGUAGES': settings.OTHER_LANGUAGES
+        'OTHER_LANGUAGES': settings.OTHER_LANGUAGES,
+                
+        'DOMAIN_PATH': ("http://" if "127." in request.get_host() else "https://") + (request.get_host()),
+
+        "CDD_URL_BASE": settings.CDD_URL_BASE,
+        "MIS_URL_BASE": settings.MIS_URL_BASE,
+        "GRM_URL_BASE": settings.GRM_URL_BASE,
     }

fbe3ce6990d6ece0df041991ec8a06322cac816d
Kodjovi Vincent ADABOUNOU <adaboubvincent@gmail.com>
2025-01-31 15:40:44 +0000

    Added function allow to know file path to download

--- src/grm/my_librairies/download_file.py
+++ src/grm/my_librairies/download_file.py
@@ -1,6 +1,7 @@
 import os
 from django.conf import settings
 from django.http import HttpResponse, Http404
+from sys import platform
 
 def download(request, path, content_type="application/pdf", param_download=True):
     file_path = os.path.join(settings.MEDIA_ROOT, path)
@@ -22,3 +23,22 @@ def download_file(data, file_name_with_extension, content_type="application/pdf"
         content = "attachment; filename=" + file_name_with_extension
     response['Content-Disposition'] = content
     return response
+
+
+
+def download_file_view(request, path, content_type):
+    if path:
+        if platform == "win32":
+            # windows
+            path = path.replace("___", "\\")
+        else:
+            path = path.replace('___', '/')
+            
+    if content_type in (None, 'None'):
+        content_type = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    
+    return download(
+        request, 
+        path,
+        content_type
+    )
